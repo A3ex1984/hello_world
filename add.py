49a98a32a5c1df7d72058ab02856808686c1819a
Alex Voets <voets.alexander@gmail.com>
2019-02-26 19:48:44 -0800

    python lists added

--- add.py
+++ add.py
@@ -1,3 +1,22 @@
 def add (a, b):
 	return a+b
 print add (1, 2)
+
+
+k = 17
+list = [1, 4, 10, 56, 78]
+listLength = len(list)
+
+#for i in list:
+
+print listLength
+
+
+myList=[1,7,9,3,1,2,8]
+seen = []
+for number in myList:
+    if number in seen:
+        print "Number repeated!"
+    else:
+        seen.append(number)
+	
