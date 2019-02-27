8b2e0fb0c5167a2ac49ed36550b5c8130607810e
Alex Voets <voets.alexander@gmail.com>
2019-02-26 20:18:46 -0800

    explanation for code runner extension

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,24 @@
+#To run code have code runner extension installed and hit CTRL ALT N on Windows or Control Option N on Mac
+
 message = "Hello World"
 print (message)
+
+k = 17
+list = [1, 4, 10, 56, 78]
+listLength = len(list)
+
+def adds_up(listLength, k):
+	i = 0
+	while i<listLength:
+		j = 1
+		while j<listLength:
+			if (list[i] + list[j] == k):
+				return True
+			else:
+				j = j+1
+	i = i+1
+
+print adds_up(listLength, k)
+		
+
+print listLength
--- add.py
+++ add.py
@@ -2,26 +2,6 @@ def add (a, b):
 	return a+b
 print add (1, 2)
 
-
-k = 17
-list = [1, 4, 10, 56, 78]
-listLength = len(list)
-
-def adds_up:
-	i = 0
-	while i<listLength:
-		j = 1
-		while j<=listLength:
-			if (list[i] + list[j] == k):
-				return True
-			else:
-				j++
-	i++
-		
-
-print listLength
-
-
 myList=[1,7,9,3,1,2,8]
 seen = []
 for number in myList:
