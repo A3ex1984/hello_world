b69d9b9bd2bdc80a0b0c223e5b2010216d83d0a5
Alex Voets <voets.alexander@gmail.com>
2019-03-01 09:54:24 -0800

    parenthesis on print statements for python3

--- add.py
+++ add.py
@@ -1,12 +1,11 @@
 def add (a, b):
 	return a+b
-print add (1, 2)
+print (add (1, 2))
 
 myList=[1,7,9,3,1,2,8]
 seen = []
 for number in myList:
     if number in seen:
-        print "Number repeated!"
+        print ("Number repeated!")
     else:
-        seen.append(number)
-	
+       seen.append(number)
