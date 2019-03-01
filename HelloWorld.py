47cd0b74fa7814574806b2a73f123e2854991faf
Alex Voets <voets.alexander@gmail.com>
2019-03-01 10:04:06 -0800

    refactored Coding problem 1

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,20 +3,21 @@
 message = "Hello World"
 print (message)
 
-k = 60
+k = 57
 list = [1, 4, 10, 56, 78]
-listLength = len(list)
-print (listLength)
 
-i = 0
-while i<=(listLength-2):
+def sum_equal_num (arr, num):
+	list_length = len(arr)
+	i = 0
 	j = 1
-	while j<=(listLength-1):
-		#print (i, j)
-		if (list[i]+list[j]==k):
-			print ("True")
-		j = j+1
-	
-	i = i+1
-	print ("False")
+	while (i < list_length-2):
+		while (j < list_length-1):
+			if ((arr[i]+arr[j]) == num):
+				return True
+			j = j+1
+		i = i+1
+	return False
 
+answer = sum_equal_num (list,k)
+
+print (answer)
